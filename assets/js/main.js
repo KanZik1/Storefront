61669727e9fb82114980d04e9e0fbaf49434f4ad
KanZik1 <christensson@hotmail.dk>
2024-02-13 09:32:45 +0100

    fixes

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+
+
+fetch('https://dummyjson.com/products') // Replace with your API URL
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json(); // or .text() if the response is text
+    })
+    .then(data => {
+        console.log(data); // Process and use the data here
+    });
